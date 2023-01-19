030d678cf733426e3d5d4ad86073469da2239cf9
nohjunh <nsh042777@naver.com>
2023-01-19 18:23:10 +0900

    Add setUpWithNavControlle (::MainActivity)

--- app/src/main/java/com/nohjunh/airpollutionservice/view/main/MainActivity.kt
+++ app/src/main/java/com/nohjunh/airpollutionservice/view/main/MainActivity.kt
@@ -2,14 +2,26 @@ package com.nohjunh.airpollutionservice.view.main
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
 import com.nohjunh.airpollutionservice.R
+import com.nohjunh.airpollutionservice.databinding.ActivityMainBinding
 import timber.log.Timber
 
 class MainActivity : AppCompatActivity() {
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
 
+    private lateinit var binding : ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
         Timber.tag("MainActivity").e("start")
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val bottomNavView = binding.bottomNavView
+        val navController = findNavController(R.id.fragmentContainerView)
+
+        bottomNavView.setupWithNavController(navController)
+
     }
 }
